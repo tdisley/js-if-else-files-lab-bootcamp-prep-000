add3e1a964bda2e7d260e3d269ca852494eaaf18
t dis <thomdisley@gmail.com>
2017-09-14 09:45:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var name = "Joe"
+var name = "susan"
 var height = "74"
 
 
 // Don't worry about this
 module.exports = { name, height
 }
-
+if (name === "susan") {
+  console.log("correct!")
+}
+  else {
+    console.log("not susan")
+  }
